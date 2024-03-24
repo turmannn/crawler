a005e59af5141b021dc5ef0834070be2ff19a22c
Artsem Belavusau <artsem.belavusau@dh.com>
2024-03-24 18:38:46 -0400

    refactor app sctructure

--- src/types.ts
+++
@@ -1,9 +0,0 @@
-export type Store = 'amazon' | 'ebay'
-
-export interface Action {
-    storeName: Store
-    url: string
-    steps: (() => Promise<void>)[];
-    getProductName: () => Promise<string>;
-    getPrice: () => Promise<string>;
-}
